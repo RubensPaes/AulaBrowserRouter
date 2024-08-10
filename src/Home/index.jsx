dcb254c36ab9ff191a1c906b29f8f42fb745a3e6
RubensPaes <rubenspaesneto@gmail.com>
2024-08-10 17:17:21 -0400

    carousel
    carousel

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,10 +1,23 @@
 import Header from "../componentes/Header";
-
+import { Carousel } from "react-responsive-carousel";
 export default function Home(){
     return(
         <>
+        <Carousel
+        infiniteLoop
+        useKeyboardArrows
+        autoPlay
+        showArrows={true}
+        showStatus={false}
+        showThumbs={false}
+        dynamicHeight
+        >
+        <div>
+            <img src=""/>
+        </div>
+
+        </Carousel>
         <Header/>
-        <h1>hvdhsabd</h1>
         </>
     )
 }
