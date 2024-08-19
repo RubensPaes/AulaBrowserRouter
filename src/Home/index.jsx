1dc91d19f6a437c0476dea1e1207cf3606417ef4
RubensPaes <rubenspaesneto@gmail.com>
2024-08-19 11:00:18 -0400

    19/08
    feito o footer

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,4 +1,5 @@
 import Header from "../componentes/Header";
+import Footer from "../componentes/Footer";
 import ListarProdutos from '../componentes/ListarProdutos';
 import { useState } from "react";
 import { Carousel } from "react-responsive-carousel";
@@ -8,11 +9,11 @@ import '../global.css'
 export default function Home(){
 
   const [listaCarros, setCarros] = useState([
-    {id: 1, nome: "Fusca", preço: "R$: 50000,00", cor: "preto", imagem: "/fusca.jpeg", especificações: ["modelo: Fusca 1300", "motor: 1300", "combustivel: gasolina"]},
-    {id: 2, nome: "Del Rey", preço: "R$: 60000,00", cor: "vermelho", imagem: "/fusca.jpeg", especificações: ["modelo: Delrey guia", "motor: AP 1.8", "combustivel: gasolina"] },
-    {id: 3, nome: "Opala", preço: "R$: 54000,00", cor: "branco", imagem: "/fusca.jpeg", especificações: ["modelo: Opala Diplomata 4.1", "motor: 8 por 1", "combustivel: gasolina"]},
-    {id: 4, nome: "uno duas portas", preço: "R$: 100000,00", imagem: "/fusca.jpeg", ano: 2009, especificações: ["modelo: Uno Mille", "motor: 1.0", "combustivel: gasolina e álcoo"] },
-    {id: 5, nome: "Brasilia Amarela", preço: "R$: 35000,00", imagem: "/fusca.jpeg", ano: 1979, especificações: ["modelo: Volkswagen Brasilia", "motor: 1.6L", "combustivel: gasolina"] }
+    {id: 1, nome: "Fusca", preço: "R$: 50000,00", imagem: "/fusca.jpeg", especificações: [" modelo: Fusca 1300 ", " motor: 1300 ", " combustivel: gasolina "]},
+    {id: 2, nome: "Del Rey", preço: "R$: 60000,00", imagem: "/imgdelrey.jpeg", especificações: [" modelo: Delrey guia ", " motor: AP 1.8 ", " combustivel: gasolina "] },
+    {id: 3, nome: "Opala", preço: "R$: 54000,00", imagem: "/opala.jpeg", especificações: [" modelo: Opala Diplomata 4.1 ", " motor: 8 por 1 ", " combustivel: gasolina "]},
+    {id: 4, nome: "Uno Duas Portas", preço: "R$: 100000,00", imagem: "/unod.jpeg", ano: 2009, especificações: [" modelo: Uno Mille ", " motor: 1.0 ", " combustivel: gasolina e álcoo "] },
+    {id: 5, nome: "Brasilia Amarela", preço: "R$: 35000,00", imagem: "/brasilia.jpeg", ano: 1979, especificações: [" modelo: Volkswagen Brasilia ", " motor: 1.6L ", " combustivel: gasolina "] }
     ])
 
 
@@ -46,11 +47,12 @@ export default function Home(){
                     imagem={Carros.imagem}
                     nome={Carros.nome}
                     preço={Carros.preço}
+                    especificações={Carros.especificações}
                 />
             ))}
         </div>
     </div>
-
+            <Footer/>
         </>
     )
 }
--- src/componentes/ListarProdutos.jsx
+++ src/componentes/ListarProdutos.jsx
@@ -1,10 +1,10 @@
-export default function ListarProdutos({nome, preço, cor, imagem}){
+export default function ListarProdutos({nome, preço, imagem, especificações}){
     return(
    <div>
  <img src={imagem} alt={nome}/>
- <p>{nome}</p>
- <p>{preço}</p>
- <p>{cor}</p>
+ <p className="pdoscarros">{nome}</p>
+ <p className="pdoscarros">{preço}</p>
+<p className="pdoscarros">{especificações}</p>
    </div>
     )
 }
