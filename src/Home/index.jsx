171df596d32ce05e874a7d666011bc370794c63b
RubensPaes <rubenspaesneto@gmail.com>
2024-08-20 10:14:57 -0400

    20/08
    Feito os toques finais do trabalho como a lista dos produtos e o botão

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,26 +1,31 @@
+import React, { useState } from "react";
 import Header from "../componentes/Header";
 import Footer from "../componentes/Footer";
 import ListarProdutos from '../componentes/ListarProdutos';
-import { useState } from "react";
 import { Carousel } from "react-responsive-carousel";
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import '../global.css'
+import '../global.css';
 
-export default function Home(){
-
+export default function Home() {
   const [listaCarros, setCarros] = useState([
     {id: 1, nome: "Fusca", preço: "R$: 50000,00", imagem: "/fusca.jpeg", especificações: [" modelo: Fusca 1300 ", " motor: 1300 ", " combustivel: gasolina "]},
     {id: 2, nome: "Del Rey", preço: "R$: 60000,00", imagem: "/imgdelrey.jpeg", especificações: [" modelo: Delrey guia ", " motor: AP 1.8 ", " combustivel: gasolina "] },
     {id: 3, nome: "Opala", preço: "R$: 54000,00", imagem: "/opala.jpeg", especificações: [" modelo: Opala Diplomata 4.1 ", " motor: 8 por 1 ", " combustivel: gasolina "]},
     {id: 4, nome: "Uno Duas Portas", preço: "R$: 100000,00", imagem: "/unod.jpeg", ano: 2009, especificações: [" modelo: Uno Mille ", " motor: 1.0 ", " combustivel: gasolina e álcoo "] },
     {id: 5, nome: "Brasilia Amarela", preço: "R$: 35000,00", imagem: "/brasilia.jpeg", ano: 1979, especificações: [" modelo: Volkswagen Brasilia ", " motor: 1.6L ", " combustivel: gasolina "] }
-    ])
+  ]);
 
+  const [listadosCarros, setListadosCarros] = useState([]);
 
-    return(
-        <>
-           <Header/>
-        <Carousel
+  const adicionarCarro = (carro) => {
+    setListadosCarros([...listadosCarros, carro]);
+    alert("você adicionou um carro!")
+  };
+
+  return (
+    <>
+      <Header/>
+      <Carousel
         infiniteLoop
         useKeyboardArrows
         interval={3000}
@@ -29,30 +34,34 @@ export default function Home(){
         showStatus={false}
         showThumbs={false}
         dynamicHeight
-        >
-             <div>
-        <img src='/carroparacarousel3.jpeg' alt="imagem 1"/>
+      >
+        <div>
+          <img src='/carroparacarousel3.jpeg' alt="imagem 1"/>
         </div>
-      <div>
-      <img src='/carroparacarousel2.jpeg' alt="imagem 2"/>
-      </div>
-      <div>
-      <img src='/carroparacarousel4.jpeg' alt="imagem 3"/>
-      </div>
-        </Carousel>
-        <div className="carroprincipal">
+        <div>
+          <img src='/carroparacarousel2.jpeg' alt="imagem 2"/>
+        </div>
+        <div>
+          <img src='/carroparacarousel4.jpeg' alt="imagem 3"/>
+        </div>
+      </Carousel>
+      
+      <div className="carroprincipal">
         <div className="carrodosprodutos">
-            {listaCarros.map((Carros) => (
-                <ListarProdutos
-                    imagem={Carros.imagem}
-                    nome={Carros.nome}
-                    preço={Carros.preço}
-                    especificações={Carros.especificações}
-                />
-            ))}
+          {listaCarros.map((carro) => (
+            <ListarProdutos
+              key={carro.id}
+              imagem={carro.imagem}
+              nome={carro.nome}
+              preço={carro.preço}
+              especificações={carro.especificações}
+              adicionarCarro={() => adicionarCarro(carro)}
+            />
+          ))}
         </div>
-    </div>
-            <Footer/>
-        </>
-    )
+      </div>
+
+      <Footer/>
+    </>
+  );
 }
